16644746465578ecb288479d7ee25d3b69a248c6
Sahil <mayur.dalvi123@gmail.com>
2026-05-20 21:32:07 -0500

    Added GitHub Actions CI pipeline

--- conftest.py
+++ conftest.py
@@ -7,7 +7,7 @@ def page():
 
     with sync_playwright() as p:
 
-        browser = p.chromium.launch(headless=False)
+        browser = p.chromium.launch(headless=True)
 
         page = browser.new_page()
 
