f8ae3a103916f25068951ed30e42ecfd39ec514f
Sahil <mayur.dalvi123@gmail.com>
2026-05-20 20:47:17 -0500

    Added automatic screenshot capture on failure

--- tests/test_login.py
+++ tests/test_login.py
@@ -3,42 +3,28 @@ import os
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
-from playwright.sync_api import sync_playwright
 from pages.login_page import LoginPage
 
 
-def test_valid_login():
+def test_valid_login(page):
 
-    with sync_playwright() as p:
+    login = LoginPage(page)
 
-        browser = p.chromium.launch(headless=False)
-        page = browser.new_page()
+    login.open()
 
-        login = LoginPage(page)
+    login.login("standard_user", "secret_sauce")
 
-        login.open()
-        login.login("standard_user", "secret_sauce")
-
-        assert "inventory" in page.url
-
-        browser.close()
+    assert "inventory" in page.url
 
 
-def test_invalid_login():
+def test_invalid_login(page):
 
-    with sync_playwright() as p:
+    login = LoginPage(page)
 
-        browser = p.chromium.launch(headless=False)
-        page = browser.new_page()
+    login.open()
 
-        login = LoginPage(page)
+    login.login("wrong_user", "wrong_password")
 
-        login.open()
+    error = login.get_error_message()
 
-        login.login("wrong_user", "wrong_password")
-
-        error = login.get_error_message()
-
-        assert "Epic sadface" in error
-
-        browser.close()
+    assert "Epic sadface" in error
